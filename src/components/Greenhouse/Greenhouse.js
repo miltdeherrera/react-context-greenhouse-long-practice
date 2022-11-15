c6dfb1f8363b27424efcd4e4040549796bd3b542
Milt Deherrera <miltdeherrera@gmail.com>
2022-11-15 15:32:30 -0800

    connect Greenhouse component with Theme context

--- src/components/Greenhouse/Greenhouse.js
+++ src/components/Greenhouse/Greenhouse.js
@@ -1,16 +1,18 @@
 import dayImage from './images/greenhouse-day.jpg';
 import nightImage from './images/greenhouse-night.jpg';
+import { useTheme } from '../../context/ThemeContext';
 import './Greenhouse.css';
 
 import LightSwitch from './LightSwitch';
 import ClimateStats from './ClimateStats';
 
 function Greenhouse() {
+  const { themeName, setThemeName } = useTheme();
 
   return (
     <section>
       <img  className='greenhouse-img'
-            src={dayImage}
+            src={themeName === 'day' ? dayImage : nightImage }
             alt='greenhouse' 
       />
       <LightSwitch />
